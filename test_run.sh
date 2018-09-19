baea107f3bbde2bb627c21dd8f0efa7f355df617
Unknown <ptrick@udel.edu>
2018-09-19 12:15:58 -0400

    Updates to testing
    Update Viewer .jar

--- test_run.sh
+++ test_run.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#! /bin/sh
 
 cd run_here
 go build ../src/
-./src -inputFileName=../testScenario/Scenario_1.txt -logPosition=true -outputFileName=testing
+./src -inputFileName=../testScenario/Scenario_4.txt -logPosition=true -logGrid=true -logEnergy=true -logNodes=true -outputFileName=testing
