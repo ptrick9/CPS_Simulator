6d63199fbfd4eb9f1200f39c5d3ea1b823be1917
Unknown <ptrick@udel.edu>
2019-07-22 14:10:47 -0400

    Parallel Updated

--- scenarioRunner.py
+++ scenarioRunner.py
@@ -2,14 +2,35 @@ import multiprocessing
 import itertools
 import os
 
+'''
+-inputFileName=Scenario_3.txt
+-imageFileName=marathon_street_map.png
+-logPosition=true
+-logGrid=true
+-logEnergy=true
+-logNodes=false
+-noEnergy=true
+-sensorPath=C:/Users/patrick/Dropbox/Patrick/udel/SUMMER2019/GitSimulator/smoothed_marathon.csv
+-SquareRowCM=60
+-SquareColCM=320
+-csvMove=true
+-movementPath=marathon_street_2k.txt
+-iterations=1000
+-csvSensor=true
+-detectionThreshold=5
+-superNodes=false
+-detectionDistance=6
+-cpuprofile=event
 
+'''
 
 def runner(queue):
     while True:
         job = queue.get()
+        #print(job)
         command = "./simulator "+' '.join(job)
         print(command)
-        os.system(command)
+        #os.system(command)
         queue.task_done()
 
 if __name__ == '__main__':
@@ -17,19 +38,25 @@ if __name__ == '__main__':
     m = multiprocessing
     q = m.JoinableQueue()
 
-    #detect_threshold = ["-detectionThreshold=%f" % float(20.0+0.5*i) for i in range(0, 20, 1)]
-    scenarios = ["-inputFileName=testScenario/%s" % s for s in ['Scenario_Random_100.txt', 'Scenario_Random_200.txt', 
-     'Scenario_Random_500.txt', 'Scenario_Random_1000.txt', 
-     'Scenario_Random_2000.txt', 'Scenario_Random_5000.txt', 'Scenario_Random_10000.txt']]
 
-    error_mult = ["-errorMultiplier=%f" % s for s in [0.0, 0.1, 1.0]]
+    scenarios = ["-inputFileName=%s -imageFileName=%s -logPostion=true -logGrid=false -logEnergy=false -logNodes=false" \
+                 "-noEnergy=true -csvMove=true -iterations=1000 -superNodes=false" % (s[0], s[1]) for s in [['Scenario_3.txt', 'marathon_street_map.png']]]
 
-    runs = (list(itertools.product(*[error_mult, scenarios])))
+    row = ["-squareRow=%d" % d for d in [60, 120]]
+    col = ["-squareCXol=%d" % d for d in [320, 640]]
+    movementPath = ["-movementPath=%s" % s for s in ["marathon_street_2k.txt"]]
+    detectionThreshold = ["-detectionThreshold=%d" % d for d in[5, 6, 7]]
+    detectionDistance = ["-detectionDistance=%d" % d for d in [6, 7]]
+
+
+    runs = (list(itertools.product(*[scenarios, row, col, movementPath, detectionThreshold, detectionDistance])))
     
     x = 0
     for r in runs:
-        for i in range(50):
-            v = [r[0], r[1], "-outputFileName=big_data/Log_%d" % x, "-squareRow=100 -squareCol=100 -noEnergy=true"]
+        for i in range(10):
+            j = [zz for zz in r]
+            j.append("-outputFileName=big_data/Log_%d" % x)
+            v = j
             q.put(v)
             x+= 1
 
