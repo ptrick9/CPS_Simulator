adb24263e23fe0a826d98a6bce66db7dbdcd3d33
Tommy <tommywhite525@gmail.com>
2021-01-29 16:11:39 -0500

    Fixed bug that was preventing global reclusters
    Adding buffering led to global reclusters not occuring because serverReadyThresh was too high. Now if global recluster type > 1, server ready threshold does not need to be met. Why serverReadyThresh was not being met may be worth looking into, especially for testing global recluster type 1.

--- simulator/cps/nodes.go
+++ simulator/cps/nodes.go
@@ -466,7 +466,7 @@ func (node *NodeImpl) SendToServer(rd *Reading, tp bool){
 
 		if server.P.GlobalRecluster > 0 {
 			nodesAccountedFor := len(server.Clusters) + len(server.ClusterHeadsOf) + len(server.AloneNodes)
-			if float64(nodesAccountedFor)/float64(len(server.P.AliveValNodes)) > server.P.ServerReadyThreshold {
+			if server.P.GlobalRecluster > 1 || float64(nodesAccountedFor)/float64(len(server.P.AliveValNodes)) > server.P.ServerReadyThreshold {
 				if server.Waiting {
 					server.Waiting = false
 					server.UpdateReclusterThresholds(nodesAccountedFor)
